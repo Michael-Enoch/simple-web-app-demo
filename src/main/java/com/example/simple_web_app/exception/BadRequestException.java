74d50037b8eaf1fd99f0347282a1e14eb8e9fed6
Michael <mike.saviour08@gmail.com>
2025-11-14 11:51:14 +0100

    added default error code

--- src/main/java/com/example/simple_web_app/exception/BadRequestException.java
+++ src/main/java/com/example/simple_web_app/exception/BadRequestException.java
@@ -1,7 +1,19 @@
 package com.example.simple_web_app.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
 public class BadRequestException extends RuntimeException{
+
+    private final String code;
     public BadRequestException(String message) {
         super(message);
+        this.code = "BAD_REQUEST";
+    }
+
+    public BadRequestException(String message, String code) {
+        super(message);
+        this.code = code;
     }
 }
