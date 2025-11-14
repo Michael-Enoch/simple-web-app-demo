5c7847b49c350bfbde8a97a905d8f7ff0406b20b
Michael <mike.saviour08@gmail.com>
2025-11-14 11:35:51 +0100

    added default error code

--- src/main/java/com/example/simple_web_app/exception/ProductNotFoundException.java
+++ src/main/java/com/example/simple_web_app/exception/ProductNotFoundException.java
@@ -1,7 +1,15 @@
 package com.example.simple_web_app.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class ProductNotFoundException extends RuntimeException{
+    private final String code;
     public ProductNotFoundException(Long id) {
         super("Product with id " + id + " not found");
+        this.code = "PRODUCT_NOT_FOUND";
     }
+
 }
