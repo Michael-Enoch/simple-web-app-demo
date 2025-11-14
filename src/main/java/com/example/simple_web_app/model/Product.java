b46f8d9624a834b4098884ccf093df0915214772
Michael <mike.saviour08@gmail.com>
2025-11-14 01:55:33 +0100

    model for products

---
+++ src/main/java/com/example/simple_web_app/model/Product.java
@@ -0,0 +1,35 @@
+package com.example.simple_web_app.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String prodName;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
