583e3ff2835b3de773d8f463b76a56bc6c4b2088
Michael <mike.saviour08@gmail.com>
2025-11-27 18:28:52 +0100

    created update service method

--- src/main/java/com/example/simple_web_app/service/ProductService.java
+++ src/main/java/com/example/simple_web_app/service/ProductService.java
@@ -2,7 +2,7 @@ package com.example.simple_web_app.service;
 
 import com.example.simple_web_app.dto.ProductRequest;
 import com.example.simple_web_app.dto.ProductResponse;
-import com.example.simple_web_app.model.Product;
+import com.example.simple_web_app.dto.UpdateProductRequest;
 
 import java.util.List;
 
@@ -10,5 +10,5 @@ public interface ProductService {
     List<ProductResponse> getProducts();
     ProductResponse getProductById(Long id);
     ProductResponse createProduct(ProductRequest request);
-
+    ProductResponse updateProduct(UpdateProductRequest request, Long id);
 }
