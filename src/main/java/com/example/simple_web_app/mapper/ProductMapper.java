826975ece017342c4ef9ce0d7706355d8c747798
Michael <mike.saviour08@gmail.com>
2025-11-27 16:43:14 +0100

    added method for updating products

--- src/main/java/com/example/simple_web_app/mapper/ProductMapper.java
+++ src/main/java/com/example/simple_web_app/mapper/ProductMapper.java
@@ -2,12 +2,15 @@
 
     import com.example.simple_web_app.dto.ProductRequest;
     import com.example.simple_web_app.dto.ProductResponse;
+    import com.example.simple_web_app.dto.UpdateProductRequest;
     import com.example.simple_web_app.model.Product;
     import org.mapstruct.Mapper;
+    import org.mapstruct.MappingTarget;
 
     @Mapper(componentModel = "spring")
     public interface ProductMapper {
         Product toEntity(ProductRequest dto);
         ProductResponse toResponse(Product entity);
 
+        void updateProductFromRequest(UpdateProductRequest request, @MappingTarget Product entity);
     }
