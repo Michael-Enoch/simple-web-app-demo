a7a5671e413f04f444ce7961e57853b48efae043
Michael <mike.saviour08@gmail.com>
2025-11-27 18:20:31 +0100

    added details field

--- src/main/java/com/example/simple_web_app/exception/ApiError.java
+++ src/main/java/com/example/simple_web_app/exception/ApiError.java
@@ -1,13 +1,17 @@
 package com.example.simple_web_app.exception;
 
+import lombok.Builder;
+
 import java.time.LocalDateTime;
 
+@Builder
 public record ApiError(
         LocalDateTime timestamp,
         int status,
         String error,
         String message,
         String path,
-        String code
+        String code,
+        Object details
 ) {
 }
