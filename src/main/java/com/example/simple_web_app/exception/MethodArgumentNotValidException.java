6234dbd81d0a64639b6a7631b13f76c6d10941ff
Michael <mike.saviour08@gmail.com>
2025-11-27 18:27:16 +0100

    added

--- src/main/java/com/example/simple_web_app/exception/MethodArgumentNotValidException.java
+++ src/main/java/com/example/simple_web_app/exception/MethodArgumentNotValidException.java
@@ -3,14 +3,14 @@ package com.example.simple_web_app.exception;
 import lombok.Getter;
 
 @Getter
-public class ValidationException extends RuntimeException {
+public class MethodArgumentNotValidException extends RuntimeException {
     private final String code;
-    public ValidationException(String message) {
+    public MethodArgumentNotValidException(String message) {
         super(message);
         this.code = "VALIDATION_ERROR";
     }
 
-    public ValidationException(String message, String code) {
+    public MethodArgumentNotValidException(String message, String code) {
         super(message);
         this.code = code;
     }
