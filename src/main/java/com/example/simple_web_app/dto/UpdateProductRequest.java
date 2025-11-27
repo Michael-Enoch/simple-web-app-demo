6f37399a26ca939bd8e7d78e2de00989ac8e2aee
Michael <mike.saviour08@gmail.com>
2025-11-27 16:41:45 +0100

    added record for update request

---
+++ src/main/java/com/example/simple_web_app/dto/UpdateProductRequest.java
@@ -0,0 +1,20 @@
+package com.example.simple_web_app.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record UpdateProductRequest(
+        @NotBlank(message = "Name cannot be blank")
+        @Size(max = 50, message = "Name must not exceed 50 characters")
+        String prodName,
+
+        @NotBlank(message = "Description cannot be blank")
+        @Size(max = 200, message = "Description must not exceed 200 characters")
+        String description,
+        @NotNull(message = "Price cannot be null")
+        @Min(value = 1, message = "Price must be at least 1")
+        Double price
+) {
+}
