00b45571cb8da1a0070c1f762a1f4282dd686d2e
Michael <mike.saviour08@gmail.com>
2025-11-27 21:56:49 +0100

    added method for sending partial updates

--- src/main/java/com/example/simple_web_app/mapper/ProductMapper.java
+++ src/main/java/com/example/simple_web_app/mapper/ProductMapper.java
@@ -1,16 +1,20 @@
     package com.example.simple_web_app.mapper;
 
+    import com.example.simple_web_app.dto.PatchProductRequest;
     import com.example.simple_web_app.dto.ProductRequest;
     import com.example.simple_web_app.dto.ProductResponse;
     import com.example.simple_web_app.dto.UpdateProductRequest;
     import com.example.simple_web_app.model.Product;
     import org.mapstruct.Mapper;
     import org.mapstruct.MappingTarget;
+    import org.mapstruct.NullValuePropertyMappingStrategy;
 
-    @Mapper(componentModel = "spring")
+    @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     public interface ProductMapper {
         Product toEntity(ProductRequest dto);
         ProductResponse toResponse(Product entity);
 
         void updateProductFromRequest(UpdateProductRequest request, @MappingTarget Product entity);
+
+        void patchProductFromRequest(PatchProductRequest request, @MappingTarget Product entity);
     }
--- src/main/java/com/example/simple_web_app/service/ProductService.java
+++ src/main/java/com/example/simple_web_app/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.example.simple_web_app.service;
 
+import com.example.simple_web_app.dto.PatchProductRequest;
 import com.example.simple_web_app.dto.ProductRequest;
 import com.example.simple_web_app.dto.ProductResponse;
 import com.example.simple_web_app.dto.UpdateProductRequest;
@@ -11,4 +12,6 @@ public interface ProductService {
     ProductResponse getProductById(Long id);
     ProductResponse createProduct(ProductRequest request);
     ProductResponse updateProduct(UpdateProductRequest request, Long id);
+
+    ProductResponse patchProduct(PatchProductRequest request, Long id);
 }
